8f553cab68a4ba576087879db497bf2cbe955b38
stqc <42732801+stqc@users.noreply.github.com>
2019-11-25 19:51:08 +0530

    Update JOC_W3P3.py

--- JOC_W3P3.py
+++ JOC_W3P3.py
@@ -1,4 +1,6 @@
-#You are given a number A which contains only digits 0's and 1's. Your task is to make all digits same by just flipping one digit (i.e. 0 to 1 or 1 to 0 ) only. If it is possible to make all the  digits same by just flipping one digit then print 'YES' else print 'NO'.
+#You are given a number A which contains only digits 0's and 1's. 
+#Your task is to make all digits same by just flipping one digit (i.e. 0 to 1 or 1 to 0 ) only. 
+#If it is possible to make all the  digits same by just flipping one digit then print 'YES' else print 'NO'.
 
 number=input()
 new_list=[]
